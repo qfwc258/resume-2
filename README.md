777cc2d40e01ab8c831b6372fa64af5a8d93d5e9
黄敬鑫 <13824339381@139.com>
2018-10-17 00:01:29 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# 微信小程序
+
+记录学习网址和一些掉过的坑
+
+## 学习网址
+
+[腾讯小程序开发](https://developers.weixin.qq.com/miniprogram/dev/index.html)  学习小程序的官方教程，有它一个就够了。
+
+[腾讯示例源码](https://github.com/wechat-miniprogram/miniprogram-demo.git)     上面教程有提到，可以优先体验后再学习。
+
+[阿里矢量图库](http://iconfont.cn/)     非常好看的图标库，可以下载到本地再引入小程序。
+
+## 前方有坑
+
+（1）没有 p、span 标签，也不用想去找类似的标签代替，基本上有文字的地方都用 view 标签
+
+（2）莫名其妙的报错，data里面的数据都添加分号，**默认为字符串**
+
+（3）所有句末都加一个分号，避免忘记添加的情况
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,73 @@
-//index.js
-//获取应用实例
-const app = getApp()
+/** index.js **/
 
 Page({
   data: {
-    name: '黄敬鑫',
     workyear: new Date().getFullYear() - 2017,
-    workmonth: new Date().getMonth() - 6
-  }
+    workmonth: new Date().getMonth() - 6,
+
+    phone: '13824339381',
+    mail: '13824339381@139.com',
+    github: 'www.github.com / huangjingxin',
+  },
+
+  actionSheetTap() {
+    wx.showActionSheet({
+      itemList: ['呼叫', '复制'],
+      success(e) {
+        console.log(e.tapIndex)
+        if (e.tapIndex===0){
+            wx.makePhoneCall({
+              phoneNumber: '13824339381',
+              success() {
+                console.log('成功拨打电话')
+              }
+            })
+        }
+        else if (e.tapIndex === 1){
+          wx.setClipboardData({
+            data: '13824339381',
+            success() {
+              wx.showToast({
+                title: '复制成功',
+                icon: 'success',
+                duration: 1000,
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  copyMail() {
+    wx.setClipboardData({
+      data: this.data.mail,
+      success() {
+        wx.showToast({
+          title: '复制成功',
+          icon: 'success',
+          duration: 1000
+        })
+      }
+    })
+  },
+
+  copyGithub() {
+    wx.setClipboardData({
+      data: this.data.github,
+      success() {
+        wx.showToast({
+          title: '复制成功',
+          icon: 'success',
+          duration: 1000
+        })
+      }
+    })
+  },
+
+
+
+
+
+
 })
